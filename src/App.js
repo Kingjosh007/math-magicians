89630013c8dd990c2e8de8a52390de772c4805da
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2021-12-04 09:47:44 +0100

    Implement routing in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import './index.css';
+import './App.css';
+import { Route, Switch } from 'react-router-dom';
 import Calculator from './components/Calculator';
+import Quote from './components/Quote';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div>
-      <Calculator />
-    </div>
+    <>
+       <Navbar />
+       <Switch>
+            <Route exact path="/">
+               <Home />
+           </Route>
+           <Route path="/calculator">
+               <Calculator />
+           </Route>
+           <Route path="/quote">
+               <Quote />
+           </Route>
+       </Switch>
+    </>
   );
 }
 
