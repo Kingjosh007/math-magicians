1462c0bc5bca77b3eb0d5d57a6865beb45e53275
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2021-12-04 09:41:29 +0100

    Create Home component

---
+++ src/components/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Home = () => (
+  <div className="home-container">
+    <h2>Welcome to our page!</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Quasi commodi, facilis atque animi impedit praesentium id
+      veniam reiciendis quisquam laboriosam velit, molestiae consectetur
+      laudantium harum, ea dignissimos explicabo recusandae incidunt.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. ea dignissimos explicabo
+      recusandae incidunt.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Quasi commodi, lorem facilis atque animi impedit praesentium id
+      veniam reiciendis quisquam laboriosam velit, molestiae consectetur
+      laudantium harum, ea dignissimos explicabo recusandae incidunt.
+    </p>
+  </div>
+);
+export default Home;
